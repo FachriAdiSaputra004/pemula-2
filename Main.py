8d16e9b1392d60a3c54cb342ae1ebe734500ee40
FachriAdiSaputra004 <fachriadisaputra004@gmail.com>
2025-06-28 14:00:48 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+import time
+start_time = time.time()
+print("hello world")
+print("james wan")
+print("holmes")
+
+print("moriarty")
+a = 10 # ini adalah comment
+# ini adalah comment 
+print(a)
+""" james prowed (ini adalah comment multiline) """
+for i in range (1,1000):
+      a = 10
+print(time.time() - start_time, "detik ")
+#  bisa mengcompile python ke yang namanya bytecode
+# cara mengcompile, buka terminal dan tuliskkan python -m py_compile Main.py
+# python -m py_compile Main.py
